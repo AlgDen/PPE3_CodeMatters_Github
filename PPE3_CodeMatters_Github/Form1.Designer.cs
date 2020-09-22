05101a03484af865f5e8621376b30ed247f58a83
ALGUIRIEV Deni <dalguiriev@BTSINFO.EDU>
2020-09-22 14:51:33 +0200

    Ajoutez des fichiers projet.

---
+++ PPE3_CodeMatters_Github/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PPE3_CodeMatters_Github
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mission1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mission2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mission1ToolStripMenuItem,
+            this.mission2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mission1ToolStripMenuItem
+            // 
+            this.mission1ToolStripMenuItem.Name = "mission1ToolStripMenuItem";
+            this.mission1ToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.mission1ToolStripMenuItem.Text = "Mission 1";
+            // 
+            // mission2ToolStripMenuItem
+            // 
+            this.mission2ToolStripMenuItem.Name = "mission2ToolStripMenuItem";
+            this.mission2ToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.mission2ToolStripMenuItem.Text = "Mission 2";
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "menu";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mission1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mission2ToolStripMenuItem;
+    }
+}
+
