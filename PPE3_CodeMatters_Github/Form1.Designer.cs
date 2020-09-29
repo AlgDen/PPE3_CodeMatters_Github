cba09758446d3ca394f0319b00669bb76ba7706d
NGABARET <NGABARET@ST208>
2020-09-29 13:56:49 +0200

    Ajout Edmx - Nico

--- PPE3_CodeMatters_Github/Form1.Designer.cs
+++ PPE3_CodeMatters_Github/Form1.Designer.cs
@@ -28,46 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.mission1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mission2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // panel1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mission1ToolStripMenuItem,
-            this.mission2ToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Location = new System.Drawing.Point(95, 390);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(237, 1);
+            this.panel1.TabIndex = 2;
             // 
-            // mission1ToolStripMenuItem
+            // pictureBox3
             // 
-            this.mission1ToolStripMenuItem.Name = "mission1ToolStripMenuItem";
-            this.mission1ToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
-            this.mission1ToolStripMenuItem.Text = "Mission 1";
+            this.pictureBox3.Image = global::PPE3_CodeMatters_Github.Properties.Resources.lock_2;
+            this.pictureBox3.Location = new System.Drawing.Point(90, 446);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(35, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
             // 
-            // mission2ToolStripMenuItem
+            // pictureBox2
             // 
-            this.mission2ToolStripMenuItem.Name = "mission2ToolStripMenuItem";
-            this.mission2ToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
-            this.mission2ToolStripMenuItem.Text = "Mission 2";
+            this.pictureBox2.Image = global::PPE3_CodeMatters_Github.Properties.Resources.Sed_18_512;
+            this.pictureBox2.Location = new System.Drawing.Point(90, 352);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PPE3_CodeMatters_Github.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(106, 68);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Blue;
+            this.panel2.Location = new System.Drawing.Point(95, 482);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(237, 1);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnConnexion.Font = new System.Drawing.Font("MS Reference Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnexion.ForeColor = System.Drawing.Color.White;
+            this.btnConnexion.Location = new System.Drawing.Point(134, 522);
+            this.btnConnexion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(171, 44);
+            this.btnConnexion.TabIndex = 5;
+            this.btnConnexion.Text = "CONNEXION";
+            this.btnConnexion.UseVisualStyleBackColor = false;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblExit.Location = new System.Drawing.Point(182, 587);
+            this.lblExit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(64, 20);
+            this.lblExit.TabIndex = 6;
+            this.lblExit.Text = "Quitter";
+            this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtIdentifiant.Location = new System.Drawing.Point(134, 365);
+            this.txtIdentifiant.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtIdentifiant.Multiline = true;
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(198, 21);
+            this.txtIdentifiant.TabIndex = 7;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpassword.Location = new System.Drawing.Point(134, 456);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtpassword.Multiline = true;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(198, 21);
+            this.txtpassword.TabIndex = 8;
             // 
             // menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(444, 611);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtIdentifiant);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.btnConnexion);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.menu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,9 +170,15 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem mission1ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem mission2ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.Label lblExit;
+        private System.Windows.Forms.TextBox txtIdentifiant;
+        private System.Windows.Forms.TextBox txtpassword;
     }
 }
 
