3ac923a451501ffc2d31bdbafc2a4cd20e108ed0
dalguiriev <dalguiriev@ST207>
2020-10-06 16:36:01 +0200

    j

--- PPE3_CodeMatters_Github/FormConnexion.cs
+++ PPE3_CodeMatters_Github/FormConnexion.cs
@@ -6,129 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms
+using System.Windows.Forms;
 
 namespace PPE3_CodeMatters_Github
 {
-    public partial class menu : Form
+    public partial class FormConnexion : Form
     {
-        public menu()
+        public FormConnexion()
         {
-
             InitializeComponent();
-            //
-        }
-
-        private void menu_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnConnexion_Click(object sender, EventArgs e)
-        {
-            if (txtIdentifiant.Text == "demo" && txtpassword.Text == "1234")
-            {
-                
-               
-
-            }
-
-            else
-            {
-                MessageBox.Show("Identifiant ou mot de passe incorrect, réessayez");
-                txtIdentifiant.Clear();
-                txtpassword.Clear();
-                txtIdentifiant.Focus();
-            }
-        }
-
-        private void menu_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnConnexion_Click(object sender, EventArgs e)
-        {
-            if (txtIdentifiant.Text == "demo" && txtpassword.Text == "1234")
-            {
-                
-               
-
-            }
-
-            else
-            {
-                MessageBox.Show("Identifiant ou mot de passe incorrect, réessayez");
-                txtIdentifiant.Clear();
-                txtpassword.Clear();
-                txtIdentifiant.Focus();
-            }
-        }
-
-        private void menu_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnConnexion_Click(object sender, EventArgs e)
-        {
-            if (txtIdentifiant.Text == "demo" && txtpassword.Text == "1234")
-            {
-                
-               
-
-            }
-
-            else
-            {
-                MessageBox.Show("Identifiant ou mot de passe incorrect, réessayez");
-                txtIdentifiant.Clear();
-                txtpassword.Clear();
-                txtIdentifiant.Focus();
-            }
-        }
-
-        private void menu_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnConnexion_Click(object sender, EventArgs e)
-        {
-            if (txtIdentifiant.Text == "demo" && txtpassword.Text == "1234")
-            {
-                
-               
-
-            }
-
-            else
-            {
-                MessageBox.Show("Identifiant ou mot de passe incorrect, réessayez");
-                txtIdentifiant.Clear();
-                txtpassword.Clear();
-                txtIdentifiant.Focus();
-            }
         }
 
         private void menu_Load(object sender, EventArgs e)
