fb38c3481009c9a91b88edcb11398e0540e98d36
dalguiriev <dalguiriev@ST207>
2020-10-06 15:18:32 +0200

    Fusion

---
+++ PPE3_CodeMatters_Github/Modele.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE3_CodeMatters_Github
+{
+    public static class Modele
+    {
+        private static CodeMattersDBEntities maConnexion;
+        
+        public static void init()
+        {
+            /* Instantiation d’un objet de la classe typée chaine de connexion SqlConnection */
+           maConnexion = new CodeMattersDBEntities();
+        }
+
+        public static 
+    }
+}
